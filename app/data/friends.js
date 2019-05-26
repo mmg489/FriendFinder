a774c2c7b28b37fcbe02c4bbbceef1184721162e
Margarita <m_mg489@outlook.com>
2019-05-25 23:08:45 -0400

    setup for friends data JS file

---
+++ app/data/friends.js
@@ -0,0 +1,36 @@
+var friends = [
+    {
+        name: "Bob",
+        photo: "https://66.media.tumblr.com/e1ee16dd5f95ca2050b87670476fd7de/tumblr_inline_pcpaywXPg11qlr65v_500.jpg",
+        scores: [1,1,1,1,1,1,1,1,1,1]
+    },
+    {
+        name: "Bob",
+        photo: "https://66.media.tumblr.com/e1ee16dd5f95ca2050b87670476fd7de/tumblr_inline_pcpaywXPg11qlr65v_500.jpg",
+        scores: [1,1,1,1,1,1,1,1,1,1]
+    },
+    {
+        name: "Bob",
+        photo: "https://66.media.tumblr.com/e1ee16dd5f95ca2050b87670476fd7de/tumblr_inline_pcpaywXPg11qlr65v_500.jpg",
+        scores: [1,1,1,1,1,1,1,1,1,1]
+    },
+    {
+        name: "Bob",
+        photo: "https://66.media.tumblr.com/e1ee16dd5f95ca2050b87670476fd7de/tumblr_inline_pcpaywXPg11qlr65v_500.jpg",
+        scores: [1,1,1,1,1,1,1,1,1,1]
+    },
+    {
+        name: "Bob",
+        photo: "https://66.media.tumblr.com/e1ee16dd5f95ca2050b87670476fd7de/tumblr_inline_pcpaywXPg11qlr65v_500.jpg",
+        scores: [1,1,1,1,1,1,1,1,1,1]
+    },
+    {
+        name: "Bob",
+        photo: "https://66.media.tumblr.com/e1ee16dd5f95ca2050b87670476fd7de/tumblr_inline_pcpaywXPg11qlr65v_500.jpg",
+        scores: [1,1,1,1,1,1,1,1,1,1]
+    },
+];
+
+module.exports = friends;
+
+]
