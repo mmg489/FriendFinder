6bf61af27a83ef0b35df0bc958b1150ee2a04333
Margarita <m_mg489@outlook.com>
2019-05-27 01:21:29 -0400

    updates to the array in friends.js

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,33 +1,33 @@
 var friends = [
     {
-        name: "Bob",
+        name: "Ross",
         photo: "https://66.media.tumblr.com/e1ee16dd5f95ca2050b87670476fd7de/tumblr_inline_pcpaywXPg11qlr65v_500.jpg",
-        scores: [1,1,1,1,1,1,1,1,1,1]
+        scores: [2,5,3,3,2,4,4,5,5,4]
     },
     {
-        name: "Bob",
+        name: "Rachel",
         photo: "https://66.media.tumblr.com/e1ee16dd5f95ca2050b87670476fd7de/tumblr_inline_pcpaywXPg11qlr65v_500.jpg",
-        scores: [1,1,1,1,1,1,1,1,1,1]
+        scores: [3,3,5,2,2,2,4,4,4,4]
     },
     {
-        name: "Bob",
+        name: "Joey",
         photo: "https://66.media.tumblr.com/e1ee16dd5f95ca2050b87670476fd7de/tumblr_inline_pcpaywXPg11qlr65v_500.jpg",
-        scores: [1,1,1,1,1,1,1,1,1,1]
+        scores: [4,1,4,5,4,2,4,2,1,5]
     },
     {
-        name: "Bob",
+        name: "Monica",
         photo: "https://66.media.tumblr.com/e1ee16dd5f95ca2050b87670476fd7de/tumblr_inline_pcpaywXPg11qlr65v_500.jpg",
-        scores: [1,1,1,1,1,1,1,1,1,1]
+        scores: [4,4,5,5,4,2,5,2,5,4]
     },
     {
-        name: "Bob",
+        name: "Chandler",
         photo: "https://66.media.tumblr.com/e1ee16dd5f95ca2050b87670476fd7de/tumblr_inline_pcpaywXPg11qlr65v_500.jpg",
-        scores: [1,1,1,1,1,1,1,1,1,1]
+        scores: [4,4,2,3,2,5,5,1,1,1]
     },
     {
-        name: "Bob",
+        name: "Phoebe",
         photo: "https://66.media.tumblr.com/e1ee16dd5f95ca2050b87670476fd7de/tumblr_inline_pcpaywXPg11qlr65v_500.jpg",
-        scores: [1,1,1,1,1,1,1,1,1,1]
+        scores: [5,3,4,2,5,1,3,1,3,5]
     },
 ];
 
