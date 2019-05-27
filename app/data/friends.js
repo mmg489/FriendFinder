56541379f422cc2dc2fa7728e579feadc6a6e499
Margarita <m_mg489@outlook.com>
2019-05-27 01:35:25 -0400

    added images for friends.js file :q

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,36 +1,35 @@
 var friends = [
     {
         name: "Ross",
-        photo: "https://66.media.tumblr.com/e1ee16dd5f95ca2050b87670476fd7de/tumblr_inline_pcpaywXPg11qlr65v_500.jpg",
+        photo: "https://i.gifer.com/z02.gif",
         scores: [2,5,3,3,2,4,4,5,5,4]
     },
     {
         name: "Rachel",
-        photo: "https://66.media.tumblr.com/e1ee16dd5f95ca2050b87670476fd7de/tumblr_inline_pcpaywXPg11qlr65v_500.jpg",
+        photo: "https://media3.giphy.com/media/uKASpvYgR7EQM/giphy.gif",
         scores: [3,3,5,2,2,2,4,4,4,4]
     },
     {
         name: "Joey",
-        photo: "https://66.media.tumblr.com/e1ee16dd5f95ca2050b87670476fd7de/tumblr_inline_pcpaywXPg11qlr65v_500.jpg",
+        photo: "https://media2.giphy.com/media/kZafx2yTA69So/giphy.gif",
         scores: [4,1,4,5,4,2,4,2,1,5]
     },
     {
         name: "Monica",
-        photo: "https://66.media.tumblr.com/e1ee16dd5f95ca2050b87670476fd7de/tumblr_inline_pcpaywXPg11qlr65v_500.jpg",
+        photo: "https://media1.giphy.com/media/SwmYRaqcowXczDPAxP/giphy.gif",
         scores: [4,4,5,5,4,2,5,2,5,4]
     },
     {
         name: "Chandler",
-        photo: "https://66.media.tumblr.com/e1ee16dd5f95ca2050b87670476fd7de/tumblr_inline_pcpaywXPg11qlr65v_500.jpg",
+        photo: "https://data.whicdn.com/images/243468490/original.gif",
         scores: [4,4,2,3,2,5,5,1,1,1]
     },
     {
         name: "Phoebe",
-        photo: "https://66.media.tumblr.com/e1ee16dd5f95ca2050b87670476fd7de/tumblr_inline_pcpaywXPg11qlr65v_500.jpg",
+        photo: "https://media1.giphy.com/media/jxW1ntywvsses/giphy.gif",
         scores: [5,3,4,2,5,1,3,1,3,5]
     },
 ];
 
 module.exports = friends;
 
-]
